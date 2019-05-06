a522f1d703cda1074bf96f3d449fb216e67fb39b
akkidgr8 <akshaysehgal42@gmail.com>
2019-05-06 10:22:26 +0530

    File type issue on addintion issue fixed

--- src/components/AddFileModal.js
+++ src/components/AddFileModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import '../sass/addFile.scss';
-import {formatDate} from '../shared/helper';
-import {withRouter} from 'react-router-dom';
+import { formatDate } from '../shared/helper';
+import { withRouter } from 'react-router-dom';
 
 function AddFileModal(props) {
     const [tab, setTab] = useState('file');
@@ -10,9 +10,9 @@ function AddFileModal(props) {
     const [createdOn, formattedDate] = formatDate(new Date());
     const [size, setSize] = useState('');
 
-    const {location} = props;
-    function create(){
-        const id = name+createdOn;
+    const { location } = props;
+    function create() {
+        const id = name + createdOn;
         const { currentDir } = location.state || { currentDir: "rootid" };
         const fileObj = {
             name,
@@ -21,20 +21,20 @@ function AddFileModal(props) {
             createdOn,
             id
         }
-        if(tab === "folder"){
+        if (tab === "folder") {
             fileObj['isFolder'] = true;
             fileObj['children'] = {};
-        }else{
-            fileObj['filetype'] = name.lastIndexOf(".")?'txt':name.slice(name.lastIndexOf(".")+1) ;
+        } else {
+            fileObj['filetype'] = name.lastIndexOf(".") === -1 ? 'txt' : name.slice(name.lastIndexOf(".") + 1);
         }
         props.onAdd({
-            [id]:fileObj
-        },currentDir);
+            [id]: fileObj
+        }, currentDir);
     }
     return <div className="add-directory">
         <div className="add-header">
             <label>Create New</label>
-            <span onClick={()=>{props.showHideAddFileModal(false)}}>&#x2715;</span>
+            <span onClick={() => { props.showHideAddFileModal(false) }}>&#x2715;</span>
         </div>
         <div className="tabs-cont">
             <div className="tabs">
@@ -43,24 +43,24 @@ function AddFileModal(props) {
             </div>
         </div>
         <div className="add-form">
-                <form name="addForm">
-                    <div className="form-row">
-                        <input value={name} onChange = {(e)=>{setName(e.target.value)}} type="text" placeholder="Name"/>
-                    </div>
-                    <div className="form-row">
-                        <input value={createdBy} onChange = {(e)=>{setCreatedBy(e.target.value)}} type="text" placeholder="Creator"/>
-                    </div>
-                    <div className="form-row">
-                        <input value={size} onChange = {(e)=>{setSize(e.target.value)}} type="number" placeholder="Size"/>
-                    </div>
-                    <div className="form-row">
-                        <input readOnly defaultValue={formattedDate} type="text" placeholder="Date"/>
-                    </div>
-                    <div className="form-row">
-                        <input type="button" value="Create" onClick={create}/>
-                    </div>
-                </form>
-            </div>
+            <form name="addForm">
+                <div className="form-row">
+                    <input value={name} onChange={(e) => { setName(e.target.value) }} type="text" placeholder="Name" />
+                </div>
+                <div className="form-row">
+                    <input value={createdBy} onChange={(e) => { setCreatedBy(e.target.value) }} type="text" placeholder="Creator" />
+                </div>
+                <div className="form-row">
+                    <input value={size} onChange={(e) => { setSize(e.target.value) }} type="number" placeholder="Size" />
+                </div>
+                <div className="form-row">
+                    <input readOnly defaultValue={formattedDate} type="text" placeholder="Date" />
+                </div>
+                <div className="form-row">
+                    <input type="button" value="Create" onClick={create} />
+                </div>
+            </form>
+        </div>
 
     </div>
 }
